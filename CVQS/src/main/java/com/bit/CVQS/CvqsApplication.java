0a8d257e9c93d1c5729f70c0eb0c6781c336f0f3
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-03-01 18:15:15 +0300

    Initial Commit

---
+++ CVQS/src/main/java/com/bit/CVQS/CvqsApplication.java
@@ -0,0 +1,13 @@
+package com.bit.CVQS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvqsApplication.class, args);
+	}
+
+}
---
+++ CVQS/src/test/java/com/bit/CVQS/CvqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.CVQS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
