a91118d17ad34db10bd00b87f215ed3c6c0c4c6f
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-03-14 15:19:29 +0300

    Delete Errors Have Been Solved

--- CVQS/src/main/java/com/bit/CVQS/CvqsApplication.java
+++ CVQS/src/main/java/com/bit/CVQS/CvqsApplication.java
@@ -2,8 +2,10 @@ package com.bit.CVQS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CvqsApplication {
 
 	public static void main(String[] args) {
