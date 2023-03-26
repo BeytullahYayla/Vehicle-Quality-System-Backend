978f9deafa0f89a83de7d25029903ce54d67b064
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-03-26 14:08:14 +0300

    Update CvqsApplication.java

--- CVQS/src/main/java/com/bit/CVQS/CvqsApplication.java
+++ CVQS/src/main/java/com/bit/CVQS/CvqsApplication.java
@@ -2,7 +2,10 @@ package com.bit.CVQS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
