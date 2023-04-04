5a794ec9c989f3b2ac1f21ae5bab277e9b354ba1
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-04-04 18:00:10 +0300

    TerminalFilter CRUD Operations Completed

---
+++ CVQS/src/main/java/com/bit/CVQS/resource/TerminalFilterController.java
@@ -0,0 +1,23 @@
+package com.bit.CVQS.resource;
+
+import com.bit.CVQS.core.utils.results.DataResult;
+import com.bit.CVQS.domain.Concrete.TerminalFilter;
+import com.bit.CVQS.service.Abstract.TerminalFilterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/TerminalFilters")
+public class TerminalFilterController {
+
+    @Autowired
+    private TerminalFilterService terminalFilterService;
+    @GetMapping("/getAll")
+    public DataResult<List<TerminalFilter>> getAll(){
+       return this.terminalFilterService.getAllFilters();
+    }
+}
