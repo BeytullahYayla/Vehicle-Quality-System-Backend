22ede4f5d733553cffcd25a26ebc50ad97dd41e0
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-04-04 18:20:38 +0300

    FolderArchitectureChanged

--- CVQS/src/main/java/com/bit/CVQS/service/Concrete/UserManager.java
+++ CVQS/src/main/java/com/bit/CVQS/service/Concrete/UserManager.java
@@ -7,7 +7,6 @@ import com.bit.CVQS.domain.Concrete.User;
 import com.bit.CVQS.service.Abstract.UserService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 @Service
 public class UserManager implements UserService {
