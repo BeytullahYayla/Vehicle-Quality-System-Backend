91dbfa74868d80fc903a0d120901af4153dd0cbc
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-04-06 23:18:08 +0300

    Defect Entry Services Completed

--- CVQS/src/main/java/com/bit/CVQS/service/Concrete/DefectManager.java
+++ CVQS/src/main/java/com/bit/CVQS/service/Concrete/DefectManager.java
@@ -5,7 +5,9 @@ import com.bit.CVQS.core.utils.results.Result;
 import com.bit.CVQS.core.utils.results.SuccessDataResult;
 import com.bit.CVQS.core.utils.results.SuccessResult;
 import com.bit.CVQS.dao.Abstract.DefectDao;
+import com.bit.CVQS.dao.Abstract.LocationDao;
 import com.bit.CVQS.domain.Concrete.Defect;
+import com.bit.CVQS.domain.Concrete.DefectLocation;
 import com.bit.CVQS.service.Abstract.Abstract.DefectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,9 @@ import java.util.List;
 public class DefectManager implements DefectService {
     @Autowired
     private DefectDao defactDao;
+
+    @Autowired
+    private LocationDao locationDao;
     @Override
     public DataResult<List<Defect>> getAllDefects() {
         return new SuccessDataResult<List<Defect>>(this.defactDao.findAll(),"Defects Listed");
@@ -22,6 +27,11 @@ public class DefectManager implements DefectService {
 
     @Override
     public Result add(Defect defect) {
+        for(DefectLocation defectLocation:defect.getLocations()){
+            if (this.locationDao.findById(defectLocation.getId()).isEmpty()){
+                this.locationDao.save(defectLocation);
+            }
+        }
         this.defactDao.save(defect);
 
 
