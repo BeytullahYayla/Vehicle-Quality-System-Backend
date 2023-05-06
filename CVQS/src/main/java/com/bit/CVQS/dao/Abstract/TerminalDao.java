45fcb3eeec77db9b60903ab149a1d72be1c58205
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-05-06 14:23:59 +0300

    Soft-Delete operations refactored

--- CVQS/src/main/java/com/bit/CVQS/dao/Abstract/TerminalDao.java
+++ CVQS/src/main/java/com/bit/CVQS/dao/Abstract/TerminalDao.java
@@ -1,11 +1,20 @@
 package com.bit.CVQS.dao.Abstract;
 
 import com.bit.CVQS.domain.Concrete.Terminals;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TerminalDao extends JpaRepository<Terminals,Integer> {
-    public List<Terminals> findByIsActiveTrue();
+    List<Terminals> findByIsActiveTrue();
+    @Transactional
+    @Modifying
+    @Query("UPDATE Terminals t SET t.deleted = true WHERE t.id = :id")
+    void softDelete(@Param("id") int id);
+
 
 }
