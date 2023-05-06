2fe12dbfc7c58c33cf7b08db561eff792cc917f3
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-05-06 20:28:10 +0300

    Project Architecture

---
+++ DefectServices/src/test/java/com/example/defects/DefectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.defects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
