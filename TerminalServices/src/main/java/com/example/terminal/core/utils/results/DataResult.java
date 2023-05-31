905804734b478898400d14f218b8d3a7bf2632c5
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-05-31 14:57:19 +0300

    Gateway Project Added

--- TerminalServices/src/main/java/com/example/terminal/core/utils/results/DataResult.java
+++ TerminalServices/src/main/java/com/example/terminal/core/utils/results/DataResult.java
@@ -8,8 +8,7 @@ public class DataResult<T> extends Result {//Generic Class
     public DataResult(T data,boolean success, String message) {
         super(success, message);
         this.data=data;
-        //Super provides us to invoke base classes constructors
-        // TODO Auto-generated constructor stub
+
     }
     public DataResult(T data,boolean success) {
         super(success);
