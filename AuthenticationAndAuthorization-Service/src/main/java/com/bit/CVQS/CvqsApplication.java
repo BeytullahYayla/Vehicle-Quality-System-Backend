534e13a53d20c15295f16cedb330cff02135dc48
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-06-22 11:25:59 +0300

    Gateway-Service And Docker Files Updated

--- AuthenticationAndAuthorization-Service/src/main/java/com/bit/CVQS/CvqsApplication.java
+++ AuthenticationAndAuthorization-Service/src/main/java/com/bit/CVQS/CvqsApplication.java
@@ -3,6 +3,7 @@ package com.bit.CVQS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.security.Security;
 
 @SpringBootApplication()
-
+@EnableDiscoveryClient
 public class CvqsApplication {
 
 	public static void main(String[] args) {
